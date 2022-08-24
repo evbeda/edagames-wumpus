9c916164c7953e3eebfea0c63343fac4e82b0801
Mario Merida <mamerida2018@gmail.com>
2022-08-24 15:27:57 -0300

    game class create

--- game/game.py
+++ game/game.py
@@ -1,2 +1,10 @@
-class Wumpus():
-    pass
+from constans.constants_game import LARGE
+from game.cell import Cell
+
+
+class WumpusGame():
+
+    def __init__(self) -> None:
+        self._board = [[Cell() for j in range(LARGE)] for i in range(LARGE)]
+        self.player_1 = None
+        self.player_2 = None

73c3be1cfe43e123b5e1a961b0446e258126ffe9
agustinaguero97 <agustin1997aguero@gmail.com>
2022-08-24 17:14:22 -0300

    typo in cell and update score,arrow methods added

--- game/player.py
+++ game/player.py
@@ -10,3 +10,9 @@ class Player():
         self.name = name
         self.score = INITIAL_SCORE
         self.arrows = INITIAL_ARROWS
+
+    def update_score(self, new_score):
+        self.score += new_score
+
+    def update_arrows(self, new_arrow_amt):
+        self.arrows += new_arrow_amt

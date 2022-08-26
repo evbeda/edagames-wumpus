7e59ce35a0fc8bbd802d5777d4ad75caf0f6a739
Matias Bertani <matiasbertani@gmail.com>
2022-08-26 15:43:09 -0300

    covnert cell to string

--- game/character.py
+++ game/character.py
@@ -1,6 +1,9 @@
+from game.player import Player
+
+
 class Character:
 
-    def __init__(self, player) -> None:
+    def __init__(self, player: Player) -> None:
         self.golds = 0
         self.diamonds = 0
         self.player = player

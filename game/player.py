ba1b88779d9294a598076024bed3d9ec026d49b5
agustinaguero97 <agustin1997aguero@gmail.com>
2022-08-26 17:07:09 -0300

    [EDA-1475] - strike - and bug bix

--- game/player.py
+++ game/player.py
@@ -10,9 +10,13 @@ class Player():
         self.name = name
         self.score = INITIAL_SCORE
         self.arrows = INITIAL_ARROWS
+        self.strike_counter = 0
 
     def update_score(self, new_score):
         self.score += new_score
 
     def update_arrows(self, new_arrow_amt):
         self.arrows += new_arrow_amt
+
+    def strike_counter_increment(self):
+        self.strike_counter += 1

a23e289957b9e450700d4cf73b3dde53c9d48c5a
natilou <natali.fpaura@gmail.com>
2022-09-27 14:47:02 -0300

    create handler interface and action class

---
+++ application/handler.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class Handler(ABC):
+    """
+    The Handler interface declares a method for building the chain of handlers.
+    It also declares a method for executing a request.
+    """
+
+    def set_next(self, handler):
+        pass
+
+    @abstractmethod
+    def execute(row, col, direction, current_player, board):
+        pass
